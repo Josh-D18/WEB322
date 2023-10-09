94cf8d7be4e1464053bf020fed4068523b7a60d9
Josh <jjdate@live.com>
2023-10-09 02:27:33 -0400

    finished login page

--- assignment1/pages/bank.js
+++ assignment1/pages/bank.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router()
 
 router.get("/", (req, res) => {
-    res.send('GET request to the homepage')
+    const user = req;
+    console.log(user);
+    res.send('GET request to the homepage');
 });
 
 
--- assignment1/pages/login.js
+++ assignment1/pages/login.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router()
+const users = require("../user.json");
 
-router.get("/", (req, res) => {
-    res.send('GET request to the homepage')
+
+
+router.get("/", (_, res) => {
+    res.render('index', users);
 });
+
+router.post("/", (req, res) => {
+    const {username, password} = req.body;
+    if (username === "") {
+        res.status(400).render('index', { errorUsername: "Error! Please enter a username!" });
+    } else if (password === "") {
+        res.status(400).render('index', { errorPassword: "Error! Please enter a password!" });
+    } else if (!users.hasOwnProperty(username)) {
+        res.status(400).render('index', { errorUsername: "Not a registered username" });
+    } else if (users[username] !== password) {
+        res.status(400).render('index', { errorPassword: "Incorrect Password" });
+    } else {
+        res.redirect("/bank")
+    }
+})
 
 module.exports = router;
